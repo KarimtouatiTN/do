f2d3d7f663d945bf58c9dabe8d5fc2d0d2266e6c
DESKTOP-OPG3T2J\Ahmed <khemiriahmed6@gmail.com>
2020-07-10 14:25:08 -0700

    js

--- themes/custom/pharmacie_theme/js/global.js
+++ themes/custom/pharmacie_theme/js/global.js
@@ -3,12 +3,36 @@
  * Global utilities.
  *
  */
+ 
+ 
+ 
+ 
+ (function ($) {
+	 
+if ($('.path-frontpage').length>0){
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+}
+	 
+	 
+	 })(jQuery);
+
 (function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.pharmacie_theme = {
     attach: function (context, settings) {
+
 
     }
   };
