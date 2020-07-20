78747abd01f19147c406d9dbbb9126ae9bfc3cb5
DESKTOP-OPG3T2J\Ahmed <khemiriahmed6@gmail.com>
2020-07-20 13:40:24 +0100

    header

--- themes/custom/pharmacie_theme/js/global.js
+++ themes/custom/pharmacie_theme/js/global.js
@@ -33,6 +33,24 @@ setTimeout( function() {
 });
 */
 
+
+$(".btn_container").click(function(){
+  $(".over_lay").css({
+    width: "100%"
+  });
+  $(".list_items_container").css({
+    left: "0px"
+  });
+});
+$(".over_lay, .close_btn").click(function(){
+  $(".over_lay").css({
+    width: "0px"
+  });
+  $(".list_items_container").css({
+    left: "-300px"
+  });
+});
+
 jQuery(".field--name-field-nom input").attr("placeholder", "Nom");
 
 jQuery(".field--name-field-email input").attr("placeholder", "E-mail");
