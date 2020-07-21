4e68208dc8bf82205d0b150dd25555dfc7c3e084
KarimtouatiTN <touatikarim7@gmail.com>
2020-07-21 16:20:01 +0100

    js bug

--- modules/custom/ph_pharmacie/js/ph_pharmacie.js
+++ modules/custom/ph_pharmacie/js/ph_pharmacie.js
@@ -1,20 +1,28 @@
 (function ($) {
 
-  if (jQuery(".block-pharmacie-edit-block").length>0){
+  if (jQuery(".block-pharmacie-edit-block").length > 0) {
     jQuery(".block-formblock-node").wrap('<div id="envoi-mdo" class="tab-pane custom-tab-pane show fade in active"></div>');
     jQuery(".block-pharmacie-edit-block").wrap('<div id="profile-form" class="tab-pane custom-tab-pane fade"></div>');
     jQuery('.custom-tab-pane').wrapAll('<div class="tab-content"></div>>');
   }
-  jQuery('#edit-chat-sender').val(jQuery('#dropdownMenuLink').html().replace(/\ /g,''))
-  jQuery('#custom-submit').attr('value',jQuery('#global-notif').val()-jQuery('#dernier-acces').val())
+
+  if (jQuery('#block-views-block-notifications-pharmacie-block-1').length > 0) {
+    jQuery('#edit-chat-sender').val(jQuery('#dropdownMenuLink').html().replace(/\ /g, ''))
+    jQuery('#custom-submit').attr('value', jQuery('#global-notif').val() - jQuery('#dernier-acces').val())
+  }
+
 
   Drupal.behaviors.pharmacie = {
     attach: function () {
-      jQuery('#global-notif').val(jQuery('#dropdownMenuLink').html().replace(/\ /g,''))
-      jQuery('#custom-submit').attr('value',jQuery('#global-notif').val()-jQuery('#dernier-acces').val())
-      jQuery('#custom-submit').click(function () {
-        jQuery('#dernier-acces').val(jQuery('#global-notif').val())
-      })
+      if (jQuery('#block-views-block-notifications-pharmacie-block-1').length > 0) {
+        jQuery('#global-notif').val(jQuery('#dropdownMenuLink').html().replace(/\ /g, ''))
+        jQuery('#custom-submit').attr('value', jQuery('#global-notif').val() - jQuery('#dernier-acces').val())
+        jQuery('#custom-submit').click(function () {
+          jQuery('#dernier-acces').val(jQuery('#global-notif').val())
+        })
+      }
 
-    }}
+
+    }
+  }
 })(jQuery);
