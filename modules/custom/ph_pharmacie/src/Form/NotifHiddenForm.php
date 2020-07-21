f4d57057628ca78c72886ed7ac3436bc338db60b
KarimtouatiTN <touatikarim7@gmail.com>
2020-07-21 03:47:48 +0100

    notif circulaire

---
+++ modules/custom/ph_pharmacie/src/Form/NotifHiddenForm.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\ph_pharmacie\Form;
+
+use Drupal\comment\Entity\Comment;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class notifHiddenForm extends FormBase
+{
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * The returned ID should be a unique string that can be a valid PHP function
+   * name, since it's used in hook implementation names such as
+   * hook_form_FORM_ID_alter().
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId()
+  {
+    return 'notif_hidden_form';
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $storage = \Drupal::entityTypeManager()->getStorage('profile');
+    $profile = $storage->loadByUser($user, 'pharmacie');
+    // Create a select field that will update the contents
+    // of the textbox below.
+    $form['chat_sender'] = [
+      '#type' => 'textfield',
+      "#id"=>'global-notif',
+      '#title' => $this->t('global'),
+    ];
+
+
+    $form['output'] = [
+      '#type' => 'textfield',
+      "#id"=>'dernier-acces',
+      '#title' => $this->t('dernier accés'),
+      '#value'=>$profile->get('field_nb_notif_dernier')->getValue()[0]['value'],
+      '#prefix' => '<div id="edit-nb">',
+      '#suffix' => '</div>',
+    ];
+
+    $form['submit'] = [
+      '#type' => 'button',
+      '#value' => $this->t('0'),
+      "#id"=>'custom-submit',
+      '#ajax' => [
+        'callback' => '::myAjaxCallback', // don't forget :: when calling a class method.
+        'event' => 'click',
+        'wrapper' => 'edit-nb', // This element is updated with this AJAX callback.
+        'progress' => [
+          'type' => 'throbber',
+          'message' => $this->t('Verifying entry...'),
+        ],
+
+      ]
+    ];
+
+    return $form;
+    // TODO: Implement buildForm() method.
+  }
+
+
+  public function myAjaxCallback(array &$form, FormStateInterface $form_state)
+  {
+    $form['output']['#value']=$form_state->getValue('chat_sender');
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $storage = \Drupal::entityTypeManager()->getStorage('profile');
+    $profile = $storage->loadByUser($user, 'pharmacie');
+    $profile->set('field_nb_notif_dernier',$form_state->getValue('chat_sender'));
+    $profile->save();
+    return $form['output'];
+  }
+
+}
---
+++ modules/custom/ph_pharmacie/src/Plugin/Block/notifFormBlock.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mariem
+ * Date: 29/01/2019
+ * Time: 09:27
+ */
+
+namespace Drupal\ph_pharmacie\Plugin\Block;
+
+//use Drupal\Console\Bootstrap\Drupal;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\profile\Entity\Profile;
+use Drupal\user\Entity\User;
+
+/**
+ * Provides a Notif hidden form Block.
+ *
+ * @Block(
+ *  id = "notif_hidden_form_block",
+ *  admin_label = @Translation("Notif hidden form Block"),
+ * )
+ */
+class notifFormBlock extends BlockBase
+{
+
+  public function build()
+  {
+    $form = \Drupal::formBuilder()->getForm('Drupal\ph_pharmacie\Form\notifHiddenForm');
+
+      return $form;
+  }
+}
