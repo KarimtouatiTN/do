39204c4a71290bf8e099752093a950dc36a75ae5
KarimtouatiTN <touatikarim7@gmail.com>
2020-07-21 03:49:04 +0100

    js pour notif

--- modules/custom/ph_pharmacie/js/ph_pharmacie.js
+++ modules/custom/ph_pharmacie/js/ph_pharmacie.js
@@ -5,10 +5,16 @@
     jQuery(".block-pharmacie-edit-block").wrap('<div id="profile-form" class="tab-pane custom-tab-pane fade"></div>');
     jQuery('.custom-tab-pane').wrapAll('<div class="tab-content"></div>>');
   }
+  jQuery('#edit-chat-sender').val(jQuery('#dropdownMenuLink').html().replace(/\ /g,''))
+  jQuery('#custom-submit').attr('value',jQuery('#global-notif').val()-jQuery('#dernier-acces').val())
 
   Drupal.behaviors.pharmacie = {
     attach: function () {
-      // alert("hello");
+      jQuery('#global-notif').val(jQuery('#dropdownMenuLink').html().replace(/\ /g,''))
+      jQuery('#custom-submit').attr('value',jQuery('#global-notif').val()-jQuery('#dernier-acces').val())
+      jQuery('#custom-submit').click(function () {
+        jQuery('#dernier-acces').val(jQuery('#global-notif').val())
+      })
 
     }}
 })(jQuery);
